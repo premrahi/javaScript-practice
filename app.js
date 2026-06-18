760b5b9847cb46e48531a974a8a3b6989846c49a
premrahi <premrahi234@gmail.com>
2026-06-18 15:02:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// =======================================
+// generator function
+
+
+function* generator() {
+    yield 1 ;
+    yield 2 ;
+    yield 3 ;
+    yield 4 ;
+    yield 5 ;
+}
+
+const gen = generator() ;
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().done)
+
+// =======================================
